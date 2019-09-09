8723dda308bb8ad017f810d5d63e3ad771a1ea0c
Luke Kaalim <lukekaalim@gmail.com>
2019-09-09 18:44:00 +1000

    Cover handle tests

--- test/handle.test.js
+++ test/handle.test.js
@@ -1,8 +1,26 @@
 // @flow strict
 const { expect, expectAll, assert } = require('@lukekaalim/test');
-const { handle } = require('../src/result');
+const { handle, succeed, fail } = require('../src/result');
+
+const expectHandleSuccess = expect(() => {
+  const result = succeed('Succeeded Value');
+
+  return assert('handle() should return whatever the onSuccess argument returns if the result is success',
+    handle(result, () => 'success', () => 'failure') === 'success',
+  );
+});
+
+const expectHandleFailure = expect(() => {
+  const result = fail('Failed Value');
+
+  return assert('handle() should return whatever the onFailure argument returns if the result is failure',
+    handle(result, () => 'success', () => 'failure') === 'failure',
+  );
+});
 
 const expectHandle = expectAll('handle() should allow simple and safe evaluation of result branches', [
+  expectHandleSuccess,
+  expectHandleFailure,
 ]);
 
 module.exports = {
